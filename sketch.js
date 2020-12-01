969da824965727a5806770d4630598cff17e20f4
iopzxc3363791 <71304166+iopzxc3363791@users.noreply.github.com>
2020-12-01 11:53:34 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://iopzxc3363791.github.io/FROG/model.json';
 
 
 function preload() {
